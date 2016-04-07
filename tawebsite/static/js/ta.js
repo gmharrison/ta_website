c3cd3dc4337935ce6d2c02aa7cd723c467f0c021
zamr <zamr@tutorassociates.com>
2016-04-07 16:10:48 +0000

    cleaned up js

--- tawebsite/static/js/ta.js
+++ tawebsite/static/js/ta.js
@@ -21,51 +21,14 @@ $(function(){
         $slide.show().addClass('active');
         nextSlideID = $slide.data('next');
         prevSlideID = $slide.data('previous');
-        
     }
     
     // Page load set up
+    $(".slide").each(function(e) {if (e != 0) $(this).hide();});                    // hide all slides except first
     $(".btn.survey-select").click(function(){$(this).toggleClass("active");});      // survey select buttons toggle .active on click
-    $('.next-slide-select').click(function(){nextSlideID = $(this).data('next');}); // survey select saves next slide ID
-    $(".slide").each(function(e) {if (e != 0) $(this).hide();});                    // hide all slides except first 
-
-    $("#next").click(function(){
-        
-        showSlide(nextSlideID);
-        
-        // if ($(".slide:visible").next().length != 0){
-        //     // if the slide has a next slide, show that, hide this
-        //     var nextSlide = $(".slide:visible").next().show();
-        //     nextSlide.prev().hide();
-        //     nextSlide.addClass('active');
-        // }
-        // else {
-        //     // if the slide doesn't have a next slide, show the first slide
-        //     $(".slide:visible").hide();
-        //     $(".slide:first").show();
-        // }
-        // return false;
-    });
-
-    // We don't need a generic "previous" button because
-    // we can't determine the previous slide from any current slide
-    // $("#prev").click(function(){
-    //     if ($(".slide:visible").prev().length != 0)
-    //         $(".slide:visible").prev().show().next().hide();
-    //     else {
-    //         $(".slide:visible").hide();
-    //         $(".slide:last").show();
-    //     }
-    //     return false;
-    // });
-    
-    // This back button uses data on the slide 
-    // to know which slide is the previous slide.
-    $('#back-btn').click(function(){
-        showSlide(prevSlideID);
-    });
-    
-    // Select subject on the first slide
+    $('.next-slide-select').click(function(){nextSlideID = $(this).data('next');}); // survey select saves nextSlideID
+    $("#next").click(function(){showSlide(nextSlideID);});                          // next button selects slide using nextSlideID
+    $('#back-btn').click(function(){showSlide(prevSlideID);});                      // back button selects slide using prevSlideID
     
 });
     
